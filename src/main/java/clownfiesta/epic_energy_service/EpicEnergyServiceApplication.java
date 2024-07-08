1e684fe5d36baa0d449c63ce96a799659dae3c25
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-07-08 12:20:49 +0200

    Initial commit

---
+++ src/main/java/clownfiesta/epic_energy_service/EpicEnergyServiceApplication.java
@@ -0,0 +1,13 @@
+package clownfiesta.epic_energy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clownfiesta/epic_energy_service/EpicEnergyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package clownfiesta.epic_energy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
